8ed2de2c0796cb6650d478308723f8bdaa842d00
DESKTOP-FAF3R10\Picni <martinkrogjensen@gmail.com>
2023-12-13 21:34:31 +0100

    Removed autofocus on calculation

--- components/calculation.js
+++ components/calculation.js
@@ -3,7 +3,7 @@
 export default function Calculation({ equation, onChange }) {
     return (
         <div className="group border-solid border-2 border rounded-lg">
-            <input autoFocus className="w-full p-4 rounded-lg" value={equation} onChange={(event) => onChange(event.target.value)} />
+            <input className="w-full p-4 rounded-lg" value={equation} onChange={(event) => onChange(event.target.value)} />
         </div>
     )
 }
